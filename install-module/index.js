49dbaeabad0c17ba0a6e714bb4195fbe85df3150
pritipadole1994@yahoo.com <pritipadole1994@yahoo.com>
2023-01-08 21:56:10 +0530

    implement packages
    Signed-off-by: pritipadole1994@yahoo.com <pritipadole1994@yahoo.com>

---
+++ install-module/index.js
@@ -0,0 +1,10 @@
+//command for install package.json -npm init
+
+var colors = require('colors');
+
+
+console.log("Hello, first package created".green)
+console.log('i like cake and pies'.underline.red);
+console.log('inverse the color'.inverse); // inverses the color
+
+
