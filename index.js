540b082ea9b4378596e3b9351601e0b775bd24aa
pritipadole1994@yahoo.com <pritipadole1994@yahoo.com>
2023-01-08 12:30:09 +0530

    leraning core modules
    Signed-off-by: pritipadole1994@yahoo.com <pritipadole1994@yahoo.com>

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const fs = require('fs');
+// node Js Tutorial contain basic topics implement core modules
+console.log("Hello Node");
+fs.writeFileSync("hello.txt","this is file cereated by fs");
+fs.writeFileSync("hello2.txt","another file created");
+
+//show name of directory
